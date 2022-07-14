8accb782a043edd678e378caa9cf4264e9825498
家毅 廖 <ducklorl815@gmail.com>
2022-07-14 20:35:58 +0800

    dwdwd

--- prjAdoDotNetDemo/Form1.Designer.cs
+++ prjAdoDotNetDemo/Form1.Designer.cs
@@ -36,6 +36,15 @@ namespace prjAdoDotNetDemo
             this.btnAdmin = new System.Windows.Forms.Button();
             this.txrNum = new System.Windows.Forms.TextBox();
             this.labSelect = new System.Windows.Forms.Label();
+            this.txtAddress = new System.Windows.Forms.TextBox();
+            this.txtEmail = new System.Windows.Forms.TextBox();
+            this.txtPhone = new System.Windows.Forms.TextBox();
+            this.txtName = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.listName = new System.Windows.Forms.ListBox();
             btnDelete = new System.Windows.Forms.Button();
             btnUpdate = new System.Windows.Forms.Button();
             btnSelect = new System.Windows.Forms.Button();
@@ -43,47 +52,52 @@ namespace prjAdoDotNetDemo
             // 
             // btnInsert
             // 
-            this.btnInsert.Location = new System.Drawing.Point(465, 32);
+            this.btnInsert.Font = new System.Drawing.Font("微軟正黑體", 12F);
+            this.btnInsert.Location = new System.Drawing.Point(12, 144);
             this.btnInsert.Name = "btnInsert";
             this.btnInsert.Size = new System.Drawing.Size(112, 55);
             this.btnInsert.TabIndex = 0;
-            this.btnInsert.Text = "Insert";
+            this.btnInsert.Text = "新增";
             this.btnInsert.UseVisualStyleBackColor = true;
             this.btnInsert.Click += new System.EventHandler(this.button1_Click);
             // 
             // btnDelete
             // 
-            btnDelete.Location = new System.Drawing.Point(465, 125);
+            btnDelete.Font = new System.Drawing.Font("微軟正黑體", 12F);
+            btnDelete.Location = new System.Drawing.Point(12, 220);
             btnDelete.Name = "btnDelete";
             btnDelete.Size = new System.Drawing.Size(112, 55);
             btnDelete.TabIndex = 1;
-            btnDelete.Text = "Delete";
+            btnDelete.Text = "刪除";
             btnDelete.UseVisualStyleBackColor = true;
             btnDelete.Click += new System.EventHandler(this.button2_Click);
             // 
             // btnUpdate
             // 
-            btnUpdate.Location = new System.Drawing.Point(465, 225);
+            btnUpdate.Font = new System.Drawing.Font("微軟正黑體", 12F);
+            btnUpdate.Location = new System.Drawing.Point(12, 296);
             btnUpdate.Name = "btnUpdate";
             btnUpdate.Size = new System.Drawing.Size(112, 55);
             btnUpdate.TabIndex = 2;
-            btnUpdate.Text = "Update";
+            btnUpdate.Text = "修改";
             btnUpdate.UseVisualStyleBackColor = true;
             btnUpdate.Click += new System.EventHandler(this.button3_Click);
             // 
             // btnSelect
             // 
-            btnSelect.Location = new System.Drawing.Point(465, 319);
+            btnSelect.Font = new System.Drawing.Font("微軟正黑體", 12F);
+            btnSelect.Location = new System.Drawing.Point(12, 12);
             btnSelect.Name = "btnSelect";
             btnSelect.Size = new System.Drawing.Size(112, 55);
             btnSelect.TabIndex = 3;
-            btnSelect.Text = "Select";
+            btnSelect.Text = "搜尋";
             btnSelect.UseVisualStyleBackColor = true;
             btnSelect.Click += new System.EventHandler(this.button4_Click);
             // 
             // btnAdmin
             // 
-            this.btnAdmin.Location = new System.Drawing.Point(605, 32);
+            this.btnAdmin.Font = new System.Drawing.Font("微軟正黑體", 12F);
+            this.btnAdmin.Location = new System.Drawing.Point(12, 372);
             this.btnAdmin.Name = "btnAdmin";
             this.btnAdmin.Size = new System.Drawing.Size(112, 55);
             this.btnAdmin.TabIndex = 4;
@@ -93,26 +107,122 @@ namespace prjAdoDotNetDemo
             // 
             // txrNum
             // 
-            this.txrNum.Location = new System.Drawing.Point(623, 337);
+            this.txrNum.Font = new System.Drawing.Font("微軟正黑體", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.txrNum.Location = new System.Drawing.Point(44, 88);
             this.txrNum.Name = "txrNum";
-            this.txrNum.Size = new System.Drawing.Size(69, 22);
+            this.txrNum.Size = new System.Drawing.Size(52, 35);
             this.txrNum.TabIndex = 5;
             this.txrNum.Text = "10";
             // 
             // labSelect
             // 
             this.labSelect.AutoSize = true;
-            this.labSelect.Location = new System.Drawing.Point(23, 19);
+            this.labSelect.Font = new System.Drawing.Font("微軟正黑體", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.labSelect.Location = new System.Drawing.Point(512, 15);
             this.labSelect.Name = "labSelect";
-            this.labSelect.Size = new System.Drawing.Size(33, 12);
+            this.labSelect.Size = new System.Drawing.Size(41, 20);
             this.labSelect.TabIndex = 6;
-            this.labSelect.Text = "label1";
+            this.labSelect.Text = "筆數";
+            // 
+            // txtAddress
+            // 
+            this.txtAddress.Font = new System.Drawing.Font("微軟正黑體", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.txtAddress.ImeMode = System.Windows.Forms.ImeMode.NoControl;
+            this.txtAddress.Location = new System.Drawing.Point(199, 144);
+            this.txtAddress.Name = "txtAddress";
+            this.txtAddress.Size = new System.Drawing.Size(121, 33);
+            this.txtAddress.TabIndex = 15;
+            // 
+            // txtEmail
+            // 
+            this.txtEmail.Font = new System.Drawing.Font("微軟正黑體", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.txtEmail.ImeMode = System.Windows.Forms.ImeMode.NoControl;
+            this.txtEmail.Location = new System.Drawing.Point(199, 99);
+            this.txtEmail.Name = "txtEmail";
+            this.txtEmail.Size = new System.Drawing.Size(121, 33);
+            this.txtEmail.TabIndex = 14;
+            // 
+            // txtPhone
+            // 
+            this.txtPhone.Font = new System.Drawing.Font("微軟正黑體", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.txtPhone.ImeMode = System.Windows.Forms.ImeMode.NoControl;
+            this.txtPhone.Location = new System.Drawing.Point(199, 56);
+            this.txtPhone.Name = "txtPhone";
+            this.txtPhone.Size = new System.Drawing.Size(121, 33);
+            this.txtPhone.TabIndex = 13;
+            // 
+            // txtName
+            // 
+            this.txtName.Font = new System.Drawing.Font("微軟正黑體", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.txtName.ImeMode = System.Windows.Forms.ImeMode.NoControl;
+            this.txtName.Location = new System.Drawing.Point(199, 13);
+            this.txtName.Name = "txtName";
+            this.txtName.Size = new System.Drawing.Size(121, 33);
+            this.txtName.TabIndex = 12;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("微軟正黑體", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.label3.Location = new System.Drawing.Point(145, 101);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(48, 24);
+            this.label3.TabIndex = 20;
+            this.label3.Text = "郵件";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("微軟正黑體", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.label4.Location = new System.Drawing.Point(145, 58);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(48, 24);
+            this.label4.TabIndex = 19;
+            this.label4.Text = "電話";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("微軟正黑體", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.label2.Location = new System.Drawing.Point(145, 15);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(48, 24);
+            this.label2.TabIndex = 18;
+            this.label2.Text = "姓名";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("微軟正黑體", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.label1.Location = new System.Drawing.Point(145, 147);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(48, 24);
+            this.label1.TabIndex = 17;
+            this.label1.Text = "地區";
+            // 
+            // listName
+            // 
+            this.listName.FormattingEnabled = true;
+            this.listName.ItemHeight = 12;
+            this.listName.Location = new System.Drawing.Point(337, 15);
+            this.listName.Name = "listName";
+            this.listName.Size = new System.Drawing.Size(120, 424);
+            this.listName.TabIndex = 21;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1011, 450);
+            this.Controls.Add(this.listName);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtAddress);
+            this.Controls.Add(this.txtEmail);
+            this.Controls.Add(this.txtPhone);
+            this.Controls.Add(this.txtName);
             this.Controls.Add(this.labSelect);
             this.Controls.Add(this.txrNum);
             this.Controls.Add(this.btnAdmin);
@@ -133,6 +243,15 @@ namespace prjAdoDotNetDemo
         private System.Windows.Forms.Button btnAdmin;
         private System.Windows.Forms.TextBox txrNum;
         private System.Windows.Forms.Label labSelect;
+        private System.Windows.Forms.TextBox txtAddress;
+        private System.Windows.Forms.TextBox txtEmail;
+        private System.Windows.Forms.TextBox txtPhone;
+        private System.Windows.Forms.TextBox txtName;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ListBox listName;
     }
 }
 
--- prjAdoDotNetDemo/Form1.cs
+++ prjAdoDotNetDemo/Form1.cs
@@ -22,15 +22,34 @@ namespace prjAdoDotNetDemo
         {
             string[] firstname = { "趙", "錢", "孫", "李", "周", "吳", "鄭", "王", "馮", "陳", "褚", "衛", "蔣", "沈", "韓", "楊" };
             string[] lastname = { "家豪", "志明", "俊傑", "建宏", "俊宏", "志豪", "志偉", "文雄", "金龍", "志強", "淑芬", "淑惠", "美玲", "雅婷", "美惠", "麗華", "淑娟", "淑貞", "怡君", "淑華" };
+            string[] company = {"gmail", "hotmail", "outlook", "yahoo" };
+            string[] place = { "台北", "台中", "新北", "新竹", "苗栗", "嘉義", "台南", "雲林", "宜蘭", "花蓮", "屏東", "高雄" };
+            string phone = "";
+            string email = "";
+            
+            for (int i = 0; i < 8; i++)
+            {
+                phone += rnd.Next(0, 9).ToString();
+            }
+            phone = "09" + phone;
+
+            for (int i = 0; i < rnd.Next(7, 12); i++)
+            {
+                email += (char)rnd.Next(97, 122);
+            }
+            email = email +"@" +company[rnd.Next(0 , company.Length)]+ ".com.tw";
+
+
             SqlConnection con = new SqlConnection();
             con.ConnectionString = @"Data Source=.;Initial Catalog=dbDemo;Integrated Security=True";
             con.Open();
+
             string name = firstname[rnd.Next(0, firstname.Length)] + lastname[rnd.Next(0, lastname.Length)];
             string sql = "INSERT INTO tCustomer(fName,fPhone,fEmail,fAdress,fPassword,fActived) VALUES";
             sql += $"('{name}'," +
-                "'0912031980'," +
-                "'arduous815@hotmail.com'," +
-                "'NewTaipei'," +
+                $"'{phone}'," +
+                 $"'{email}'," +
+                $"'{place[rnd.Next(0,place.Length)]}'," +
                 "'123feqwf'," +
                 "'0')";
 
@@ -41,26 +60,33 @@ namespace prjAdoDotNetDemo
             cmd.ExecuteNonQuery();
 
             con.Close();
-
+            
             MessageBox.Show("新增資料成功");
         }
 
         private void button2_Click(object sender, EventArgs e)
         {
+ 
             SqlConnection con = new SqlConnection();
             con.ConnectionString = @"Data Source=.;Initial Catalog=dbDemo;Integrated Security=True";
             con.Open();
             SqlCommand cmd = new SqlCommand();
-
+            
             cmd.Connection = con;
-            cmd.CommandText = "DELETE FROM tCustomer";
-            //cmd.CommandText = "DELETE FROM tCustomer WHERE fName='Duck'";
-            cmd.ExecuteNonQuery();
+            //cmd.CommandText = "DELETE FROM tCustomer";     $"'{phone}',"  全部都要格式化
+            cmd.CommandText = $"DELETE FROM tCustomer WHERE fName= '{listName.SelectedItem}'";
 
+
+
+            cmd.ExecuteNonQuery();
 
             con.Close();
 
+
             MessageBox.Show("刪除資料成功");
+
+
+            //listName.DataSource = listName.Items;
         }
 
         private void button3_Click(object sender, EventArgs e)
@@ -98,6 +124,7 @@ namespace prjAdoDotNetDemo
             //MessageBox.Show(Reader.VisibleFieldCount.ToString());
 
             labSelect.Text = "";
+            listName.Items.Clear();
             string s = "";
             for (int i = 0; i < int.Parse(txrNum.Text); i++)
             {
@@ -109,6 +136,7 @@ namespace prjAdoDotNetDemo
                          $"{Reader["fPhone"]}  " +
                          $"{Reader["fEmail"]}  " +
                          $"{Reader["fAdress"]}  \n");
+                    listName.Items.Add(Reader["fName"]);
                 }
                 else
                 {
